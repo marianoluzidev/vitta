a0bf3ccbf8820d6bfb3bffcd882cdcf88a7226fd
Mariano Luzi <marianoluzi@hotmail.com>
2025-12-03 10:39:07 -0300

    feat: add Firebase config (auth + firestore)

---
+++ src/firebase/app.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAp4wdB3D9EIbDzsGtEYpzvjvPwqou7yMs",
+  authDomain: "vitta-69d8b.firebaseapp.com",
+  projectId: "vitta-69d8b",
+  storageBucket: "vitta-69d8b.firebasestorage.app",
+  messagingSenderId: "440717168756",
+  appId: "1:440717168756:web:023d0d49ddd0be7ea58121",
+  measurementId: "G-H8Y6EZCFMB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
