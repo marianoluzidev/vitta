49dbd2c43d96e3fe25f2781412b57b2144900f40
Mariano Luzi <marianoluzi@hotmail.com>
2026-01-09 15:08:47 -0300

    reset: clean slate for new Vitta

--- src/types/colorthief.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'colorthief' {
-  export default class ColorThief {
-    getPalette(img: HTMLImageElement, colorCount?: number): number[][]
-    getColor(img: HTMLImageElement): number[]
-  }
-}
-
